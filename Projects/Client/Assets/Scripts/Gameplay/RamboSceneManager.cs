cd094accf142b4dfff799ea5d94f480e82403184
neo <neoparmin@live.com>
2019-08-15 21:45:13 +0430

    -minor change main menu scene added

---
+++ Projects/Client/Assets/Scripts/Gameplay/RamboSceneManager.cs
@@ -0,0 +1,46 @@
+﻿//Rambo Team
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace RamboTeam.Client
+{
+    public class RamboSceneManager : MonoBehaviorBase
+    {
+
+        public static RamboSceneManager Instance
+        {
+            get;
+            private set;
+        }
+
+        public string CurrentSceneName
+        {
+            get;
+            private set;
+        }
+
+        protected override void Awake()
+        {
+            base.Awake();
+            DontDestroyOnLoad(this.gameObject);
+            if (Instance == null)
+                Instance = this;
+            Init();
+        }
+
+        public void Init()
+        {
+            Scene scene = SceneManager.GetActiveScene();
+            CurrentSceneName = scene.name;
+        }
+
+        public void LoadScene(string Name, LoadSceneMode SceneMode)
+        {
+            SceneManager.LoadScene(Name, SceneMode);
+            SceneManager.LoadSceneAsync(Name);
+            CurrentSceneName = Name;
+
+        }
+    }
+}
---
+++ Projects/Client/Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,25 @@
+﻿//Rambo Team
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace RamboTeam.Client.UI
+{
+    public class MainMenu : MonoBehaviorBase
+    {
+        public Button SingleButton;
+        public Button CoOpButton;
+        public Button QuitButton;
+
+        protected override void Awake()
+        {
+            base.Awake();
+
+            SingleButton.onClick.AddListener(()=>RamboSceneManager.Instance.LoadScene("MainScene",UnityEngine.SceneManagement.LoadSceneMode.Single));
+            QuitButton.onClick.AddListener(() => Application.Quit());
+        }
+
+    }
+
+}
