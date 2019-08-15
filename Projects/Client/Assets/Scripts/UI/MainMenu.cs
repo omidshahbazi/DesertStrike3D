fbf5bf9e10a28af5ef57a3b7223ed49fc6382d83
neo <neoparmin@live.com>
2019-08-15 22:02:08 +0430

    -MainMusic Added

--- Projects/Client/Assets/Scripts/UI/MainMenu.cs
+++ Projects/Client/Assets/Scripts/UI/MainMenu.cs
@@ -11,12 +11,20 @@ namespace RamboTeam.Client.UI
         public Button SingleButton;
         public Button CoOpButton;
         public Button QuitButton;
+        private AudioSource MainMenuMusic;
 
         protected override void Awake()
         {
             base.Awake();
 
-            SingleButton.onClick.AddListener(()=>RamboSceneManager.Instance.LoadScene("MainScene",UnityEngine.SceneManagement.LoadSceneMode.Single));
+            MainMenuMusic = GetComponent<AudioSource>();
+            MainMenuMusic.Play();
+            SingleButton.onClick.AddListener(() =>
+            {
+                MainMenuMusic.Stop();
+                RamboSceneManager.Instance.LoadScene("MainScene", UnityEngine.SceneManagement.LoadSceneMode.Single);
+            }
+            );
             QuitButton.onClick.AddListener(() => Application.Quit());
         }
 
