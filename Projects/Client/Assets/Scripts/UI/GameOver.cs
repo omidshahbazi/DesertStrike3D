a12070e905ce0b434cebec17691831c32dcefb60
neo <neoparmin@live.com>
2019-08-16 17:18:41 +0430

    -game over added

---
+++ Projects/Client/Assets/Scripts/UI/GameOver.cs
@@ -0,0 +1,38 @@
+﻿//Rambo Team
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace RamboTeam.Client.UI
+{
+    public class GameOver : MonoBehaviorBase
+    {
+        public static GameOver Instance
+        {
+            get;
+            private set;
+        }
+
+        public Button ReturnToMainMenu;
+     
+        // Start is called before the first frame update
+        protected override void Awake()
+        {
+            base.Awake();
+
+            if (Instance == null)
+                Instance = this;     
+
+            ReturnToMainMenu.onClick.AddListener(() =>
+            {
+                this.gameObject.SetActive(false);
+                RamboSceneManager.Instance.LoadScene("MainMenu", UnityEngine.SceneManagement.LoadSceneMode.Single);
+            });
+
+            this.gameObject.SetActive(false); 
+        }
+   
+    }
+
+}
