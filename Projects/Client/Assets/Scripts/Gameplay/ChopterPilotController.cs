e0d6480ab5a75df2a166721408017368bb0d7686
neo <neoparmin@live.com>
2019-08-20 15:46:55 +0430

    -minor change in projectile

--- Projects/Client/Assets/Scripts/Gameplay/ChopterPilotController.cs
+++ Projects/Client/Assets/Scripts/Gameplay/ChopterPilotController.cs
@@ -1,176 +1,205 @@
 ﻿//Rambo Team
+using RamboTeam.Client.GamePlayLogic;
+using System;
 using UnityEngine;
 
 namespace RamboTeam.Client
 {
-	public class ChopterPilotController : MonoBehaviorBase
-	{
-		public static ChopterPilotController Instance
-		{
-			get;
-			private set;
-		}
+    public class ChopterPilotController : MonoBehaviorBase
+    {
+        public static ChopterPilotController Instance
+        {
+            get;
+            private set;
+        }
 
-		private const float SYNC_RATE = 2;
-		private const float SYNC_PERIOD = 1 / SYNC_RATE;
+        [SerializeField]
+        public GameObject MissleLuncher;
+        [SerializeField]
+        public Transform RightMissleLuncher;
+        [SerializeField]
+        public Transform LeftMissleLuncher;
+        [SerializeField]
+        public float MissleLuncherRateOfShot;
 
-		private float nextSyncTime = 0.0F;
-		private Vector3 lastPosition = Vector3.zero;
-		private Quaternion lastRotation = Quaternion.identity;
+        private float nextShotTime;
 
-		private float verticalRoation = 0;
-		private float horizontalRoation = 0;
+        private const float SYNC_RATE = 2;
+        private const float SYNC_PERIOD = 1 / SYNC_RATE;
+      
+        private float nextSyncTime = 0.0F;
+        private Vector3 lastPosition = Vector3.zero;
+        private Quaternion lastRotation = Quaternion.identity;
 
-		public float MovementSpeed = 10;
-		public float RotationSpeed = 10;
-		public float VerticalRotation = 15;
-		public float HorizontalRotation = 10;
+        private float verticalRoation = 0;
+        private float horizontalRoation = 0;
 
-		[SerializeField]
-		private GameObject ChopterModel;
+        public float MovementSpeed = 10;
+        public float RotationSpeed = 10;
+        public float VerticalRotation = 15;
+        public float HorizontalRotation = 10;
 
-		public bool IsPilot
-		{
-			get;
-			private set;
-		}
+        [SerializeField]
+        private GameObject ChopterModel;
+        private bool  nextPos;
 
-		public bool IsMoving
-		{
-			get;
-			private set;
-		}
+        public bool IsPilot
+        {
+            get;
+            private set;
+        }
 
-		protected override void Awake()
-		{
-			base.Awake();
+        public bool IsMoving
+        {
+            get;
+            private set;
+        }
 
-			Instance = this;
+        protected override void Awake()
+        {
+            base.Awake();
 
-			lastPosition = transform.position;
-		}
+            Instance = this;
 
-		protected override void OnEnable()
-		{
-			base.OnEnable();
+            lastPosition = transform.position;
+        }
 
-			NetworkCommands.OnPilot += OnPilot;
-			NetworkCommands.OnCommando += OnCommando;
-			NetworkCommands.OnSyncChopterTransform += OnSyncChopterTransform;
-		}
+        protected override void OnEnable()
+        {
+            base.OnEnable();
 
-		protected override void OnDisable()
-		{
-			base.OnDisable();
+            NetworkCommands.OnPilot += OnPilot;
+            NetworkCommands.OnCommando += OnCommando;
+            NetworkCommands.OnSyncChopterTransform += OnSyncChopterTransform;
+            InputManager.Instance.AddInput(KeyCode.Z, ShootMissle);
+        }
 
-			NetworkCommands.OnPilot -= OnPilot;
-			NetworkCommands.OnCommando -= OnCommando;
-			NetworkCommands.OnSyncChopterTransform -= OnSyncChopterTransform;
-		}
+        private void ShootMissle()
+        {
+            if (Time.time < nextShotTime)
+                return;
 
-		private void OnPilot()
-		{
-			IsPilot = true;
-		}
+            nextShotTime = Time.time + MissleLuncherRateOfShot;
+            nextPos = !nextPos;
+            Vector3 pos = nextPos  ? RightMissleLuncher.position : LeftMissleLuncher.position;
+            GameObject newObject = GameObject.Instantiate(MissleLuncher, pos, Quaternion.identity) as GameObject;
+            Projectile ps = newObject.GetComponent<Projectile>();
+            ps.InitialSetUp(pos, Vector3.right);
+        }
 
-		private void OnCommando()
-		{
-			IsPilot = false;
-		}
+        protected override void OnDisable()
+        {
+            base.OnDisable();
 
-		private void OnSyncChopterTransform(Vector3 Position, Vector3 Rotation)
-		{
-			if (IsPilot)
-				return;
+            NetworkCommands.OnPilot -= OnPilot;
+            NetworkCommands.OnCommando -= OnCommando;
+            NetworkCommands.OnSyncChopterTransform -= OnSyncChopterTransform;
+            InputManager.Instance.RemoveInput(KeyCode.Z, ShootMissle);
+        }
 
-			lastPosition = Position;
-			lastRotation = Quaternion.Euler(Rotation);
+        private void OnPilot()
+        {
+            IsPilot = true;
+        }
 
-			nextSyncTime = Time.time + SYNC_PERIOD;
-		}
+        private void OnCommando()
+        {
+            IsPilot = false;
+        }
 
-		protected override void Update()
-		{
-			base.Update();
+        private void OnSyncChopterTransform(Vector3 Position, Vector3 Rotation)
+        {
+            if (IsPilot)
+                return;
 
-			float t = Time.deltaTime;
+            lastPosition = Position;
+            lastRotation = Quaternion.Euler(Rotation);
 
-			bool isControlDown = (Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl));
-			if (isControlDown)
-			{
-				if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
-				{
-					CameraController.Instance.PanOffset += Vector3.forward;
-				}
-				if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
-				{
-					CameraController.Instance.PanOffset -= Vector3.forward;
-				}
+            nextSyncTime = Time.time + SYNC_PERIOD;
+        }
 
-				if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
-				{
-					CameraController.Instance.PanOffset += Vector3.right;
-				}
-				if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
-				{
-					CameraController.Instance.PanOffset -= Vector3.right;
-				}
-			}
+        protected override void Update()
+        {
+            base.Update();
 
-			if (IsPilot)
-			{
-				IsMoving = false;
+            float t = Time.deltaTime;
 
-				if (!isControlDown)
-				{
-					verticalRoation = 0;
-					horizontalRoation = 0;
+            bool isControlDown = (Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl));
+            if (isControlDown)
+            {
+                if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+                {
+                    CameraController.Instance.PanOffset += Vector3.forward;
+                }
+                if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+                {
+                    CameraController.Instance.PanOffset -= Vector3.forward;
+                }
 
-					if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
-					{
-						verticalRoation = VerticalRotation;
+                if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+                {
+                    CameraController.Instance.PanOffset += Vector3.right;
+                }
+                if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+                {
+                    CameraController.Instance.PanOffset -= Vector3.right;
+                }
+            }
 
-						transform.Translate(transform.forward * Time.deltaTime * MovementSpeed, Space.World);
-						IsMoving = true;
-					}
-					if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
-					{
-						verticalRoation = VerticalRotation * -1;
+            if (IsPilot)
+            {
+                IsMoving = false;
 
-						transform.Translate(transform.forward * Time.deltaTime * MovementSpeed * -1, Space.World);
-						IsMoving = true;
-					}
+                if (!isControlDown)
+                {
+                    verticalRoation = 0;
+                    horizontalRoation = 0;
 
-					if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
-					{
-						horizontalRoation = HorizontalRotation;
+                    if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+                    {
+                        verticalRoation = VerticalRotation;
 
-						transform.Rotate(0, Time.deltaTime * RotationSpeed * -1, 0, Space.World);
-					}
-					if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
-					{
-						horizontalRoation = HorizontalRotation * -1;
+                        transform.Translate(transform.forward * Time.deltaTime * MovementSpeed, Space.World);
+                        IsMoving = true;
+                    }
+                    if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+                    {
+                        verticalRoation = VerticalRotation * -1;
 
-						transform.Rotate(0, Time.deltaTime * RotationSpeed, 0, Space.World);
-					}
+                        transform.Translate(transform.forward * Time.deltaTime * MovementSpeed * -1, Space.World);
+                        IsMoving = true;
+                    }
 
-					ChopterModel.transform.localRotation = Quaternion.Lerp(ChopterModel.transform.localRotation, Quaternion.Euler(verticalRoation, 0, horizontalRoation), t);
-				}
+                    if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+                    {
+                        horizontalRoation = HorizontalRotation;
 
-				if (Time.time >= nextSyncTime)
-				{
-					NetworkCommands.SyncChopterTransform(transform.position, transform.rotation.eulerAngles);
+                        transform.Rotate(0, Time.deltaTime * RotationSpeed * -1, 0, Space.World);
+                    }
+                    if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+                    {
+                        horizontalRoation = HorizontalRotation * -1;
 
-					nextSyncTime = Time.time + SYNC_PERIOD;
-				}
-			}
-			else
-			{
-				transform.position = Vector3.Lerp(transform.position, lastPosition, t);
-				transform.rotation = Quaternion.Lerp(transform.rotation, lastRotation, t);
+                        transform.Rotate(0, Time.deltaTime * RotationSpeed, 0, Space.World);
+                    }
 
-				IsMoving = ((lastPosition - transform.position).sqrMagnitude > 1);
-			}
-		}
-	}
+                    ChopterModel.transform.localRotation = Quaternion.Lerp(ChopterModel.transform.localRotation, Quaternion.Euler(verticalRoation, 0, horizontalRoation), t);
+                }
+
+                if (Time.time >= nextSyncTime)
+                {
+                    NetworkCommands.SyncChopterTransform(transform.position, transform.rotation.eulerAngles);
+
+                    nextSyncTime = Time.time + SYNC_PERIOD;
+                }
+            }
+            else
+            {
+                transform.position = Vector3.Lerp(transform.position, lastPosition, t);
+                transform.rotation = Quaternion.Lerp(transform.rotation, lastRotation, t);
+
+                IsMoving = ((lastPosition - transform.position).sqrMagnitude > 1);
+            }
+        }
+    }
 }
