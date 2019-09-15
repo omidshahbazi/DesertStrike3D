fe720e7312655f1aaffa49d16e8c810df12360d2
neo <neoparmin@live.com>
2019-09-15 21:59:28 +0430

    -minor change

--- Projects/Client/Assets/Scripts/Gameplay/PilotCameraController.cs
+++ Projects/Client/Assets/Scripts/Gameplay/PilotCameraController.cs
@@ -8,6 +8,7 @@ namespace RamboTeam.Client
 		public static readonly Vector3 SINGLE_PLAYER_CAMERA_OFFSET_DIRECTION = new Vector3(-1, 2, -1);
 		public static readonly Vector3 MULTI_PLAYER_CAMERA_OFFSET_DIRECTION = new Vector3(0, 0.2F, -1);
 
+        [SerializeField]
 		public float Speed = 10.0F;
 		public float SingleplayerBaseDistance = 150;
 		public float MultiplayerBaseDistance = 100;
@@ -69,25 +70,36 @@ namespace RamboTeam.Client
 			forward = (forward - chopterTransform.position).normalized;
 			Vector3 targetPos = Vector3.zero;
 
-			if (RamboSceneManager.IsMultiplayer)
-				targetPos = chopterTransform.position + (forward * MultiplayerBaseDistance);
-			else
-				targetPos = chopterTransform.position + (forward * SingleplayerBaseDistance);
+            if (RamboSceneManager.IsMultiplayer)
+                targetPos = chopterTransform.position + (forward * MultiplayerBaseDistance);
+            else
+            {
 
-			if (!RamboSceneManager.IsMultiplayer && chopter.IsMoving)
-			{
-				Vector3 chopterForward = chopterTransform.forward;
-				chopterForward.y = 0;
+                    targetPos = chopterTransform.position + (forward * SingleplayerBaseDistance);
+              
+            }
+			if (!RamboSceneManager.IsMultiplayer/* && chopter.IsMoving*/)
+            {
+                Vector3 chopterForward = chopterTransform.forward;
+                chopterForward.y = 0;
 
-				float angle = Vector3.Angle(chopterForward, Vector3.right);
+                float angle = Vector3.Angle(chopterForward, Vector3.right);
 
-				if (chopterForward.z < 0)
-					angle = 360 - angle;
+                if (chopterForward.z < 0)
+                    angle = 360 - angle;
 
-				angle *= Mathf.Deg2Rad;
+                angle *= Mathf.Deg2Rad;
+                Vector3 chopterScreenPosition = Camera.main.WorldToScreenPoint(chopter.transform.position);
+                float bootomScreenPos = Screen.height / 3;
+                float offSetValue = 1F;
+                //if (chopterScreenPosition.x < bootomScreenPos || chopterScreenPosition.y < bootomScreenPos)
+                //    offSetValue = (Screen.height / (float)Screen.width);
+                //else
+                //    offSetValue = (Screen.width / (float)Screen.height);
 
-				targetPos.x += OffsetRadius * Mathf.Cos(angle) * (Screen.width / (float)Screen.height);
-				targetPos.z += OffsetRadius * Mathf.Sin(angle) * (Screen.width / (float)Screen.height);
+
+                targetPos.x += OffsetRadius * Mathf.Cos(angle) * offSetValue/* * (Screen.width / (float)Screen.height);
+				targetPos.z += OffsetRadius * Mathf.Sin(angle)* offSetValue /** (Screen.width / (float)Screen.height)*/;
 			}
 
 			float t = Time.deltaTime * Speed;
